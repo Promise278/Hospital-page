ce7e68a892f8c9d7832b5c154f79454b7c3fac44
Marvy247 <marvi@example.com>
2025-02-06 10:18:38 +0100

    I refactored the components and improve d styling for better readability and user experience

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const contact = () => {
+const Contact = () => {
   return (
-    <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sit odit quis, tempore eum aperiam nihil ut veritatis iste, ab dignissimos vitae minus praesentium obcaecati quod repudiandae fugiat eligendi modi.</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor at facilis harum aut itaque sapiente dicta, voluptas maiores voluptatum illo tempora pariatur eaque non alias hic, ex veritatis quis ea.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut numquam iusto corporis illo illum nam facilis quis, mollitia temporibus eum explicabo harum, vel voluptatum iste, consequuntur suscipit amet ratione expedita.</p>
-    </div>
-  )
-}
+    <section>
+      <h1>Contact Us</h1>
+      <p>
+        If you have any questions or need assistance, please feel free to reach
+        out to us.
+      </p>
+      <p>Email: contact@hospital.com</p>
+      <p>Phone: 08143121881</p>
+      <p>Address: 123 Hospital Lane, City, State, ZIP</p>
+    </section>
+  );
+};
 
-export default contact
+export default Contact;
